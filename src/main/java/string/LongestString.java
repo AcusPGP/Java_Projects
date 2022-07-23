c6343181181bfe6d6d621e8fd04a38fd07730850
Acus <acus1304pgphcm@gmail.com>
2022-07-23 23:22:16 +0700

    Longest String <string>

--- src/main/java/string/LongestString.java
+++ src/main/java/string/LongestString.java
@@ -4,15 +4,31 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class LongestString {
-    public Map<Character, Integer> findLongestString (String input) {
+    public Map<Character, Integer> findLongestString(String input) {
         Map<Character, Integer> map = new HashMap<>();
-        for (int i = 0; i < input.length(); i++) {
-            if (!map.containsKey(input.charAt(i))) {
-
+        int i, j;
+        for (i = 0; i < input.length(); i++) {
+            if (map.containsValue(input.charAt(i))) {
+                map.get(map.containsKey(input.charAt(i)));
+            } else {
+                map.put(input.charAt(i), i);
             }
         }
+        for (Map.Entry e : map.entrySet()) {
+            System.out.println("Key:" + e.getKey() + " and Value: " + e.getValue());
+        }
+        String convert = "";
+        for (i = 0; i < input.length(); i++) {
+            convert += map.get(input.charAt(i));
+        }
+        System.out.println(convert);
+        return map;
     }
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        String input = "phuch";
+        LongestString s = new LongestString();
+        s.findLongestString(input);
     }
 }
+
