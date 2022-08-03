a3af2f3213d568f621e38727988dcf900d5f0f9b
Acus <acus1304pgphcm@gmail.com>
2022-08-03 12:30:28 +0700

    Leetcode exercises

---
+++ src/main/java/leetcode/array/TwoSum.java
@@ -0,0 +1,55 @@
+package leetcode.array;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+    public static int[] TwoSum1(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] == target - nums[i]) {
+                    System.out.println("[" + i + ", " + j + "]");
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return null;
+    }
+
+    public static int[] TwoSum2(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+        for (int i = 0; i < nums.length; i++) {
+            int temp = target - nums[i];
+            if (map.containsKey(temp) && map.get(temp) != i) {
+                System.out.println("[" + i + ", " + map.get(temp) + "]");
+                return new int[] {i, map.get(temp)};
+            }
+        }
+        return null;
+    }
+
+    public static int[] TwoSum3(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int temp = target - nums[i];
+            if(map.containsKey(temp)) {
+                System.out.println("[" + map.get(temp) + ", " + i + "]");
+                return new int[] {map.get(temp), i};
+            }
+            map.put(nums[i], i);
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3};
+        int target = 4;
+        TwoSum1(nums, target);
+        TwoSum2(nums, target);
+        TwoSum3(nums, target);
+    }
+}
+
