b164caeb8bd4cacce7aa26413954dc6553885463
Acus <acus1304pgphcm@gmail.com>
2022-08-05 17:40:51 +0700

    Update the file: Output the result of finding the exact num

--- src/main/java/millionnames/OneMillionNamesToFile.java
+++ src/main/java/millionnames/OneMillionNamesToFile.java
@@ -1,17 +1,21 @@
 package millionnames;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 
 public class OneMillionNamesToFile {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        file();
+        int num = 999999;
+        findNumber(num);
+    }
+
+    public static void file() {
         try {
             File file = new File("/Users/macbook/IdeaProjects/Java_Projects/OneMillionNames.txt");
             if (file.createNewFile()) {
                 FileWriter writer = new FileWriter("/Users/macbook/IdeaProjects/Java_Projects/OneMillionNames.txt");
                 for (int i = 0; i < 1000000; i++) {
-                    writer.write("Nguyen Van " + i +"\n");
+                    writer.write("Nguyen Van " + i + "\n");
                 }
                 writer.close();
             }
@@ -19,5 +23,20 @@ public class OneMillionNamesToFile {
             System.out.println("An error occurred.");
             e.printStackTrace();
         }
+    }
+
+    public static void findNumber(int num) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader("/Users/macbook/IdeaProjects/Java_Projects/OneMillionNames.txt"));
+        String currentLine;
+        while ((currentLine = reader.readLine()) != null) {
+            String[] array = currentLine.split(" ");
+            int t = Integer.parseInt(array[2]);
+            if (t == num) {
+                System.out.println(currentLine + "\n");
+                reader.close();
+                break;
+            }
+        }
+        reader.close();
     }
 }
