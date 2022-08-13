8af835e3976c94237cb377db1923b14533ede0c6
Acus <acus1304pgphcm@gmail.com>
2022-08-13 18:58:31 +0700

    Update the leetcode exercises

---
+++ src/main/java/leetcode/array/RotatedSortedArray.java
@@ -0,0 +1,23 @@
+package leetcode.array;
+
+import java.util.HashMap;
+
+public class RotatedSortedArray {
+    public static int search(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(i, 1);
+            if(nums[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {4, 5, 6, 7, 0 ,1 ,2};
+        int target = 2;
+        int outPut = search(nums, target);
+        System.out.println("Output: " + outPut);
+    }
+}
---
+++ src/main/java/leetcode/array/_3Sum.java
@@ -0,0 +1,34 @@
+package leetcode.array;
+
+import java.util.*;
+
+public class _3Sum {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        Set <List<Integer>> triplets = new HashSet<>();
+        for (int left = 0; left < nums.length - 2; left++) {
+            int mid = left + 1;
+            int right = nums.length - 1;
+            while (mid < right) {
+                int sum = nums[left] + nums[mid] + nums[right];
+                if (sum < 0) {
+                    mid++;
+                } else if (sum > 0) {
+                    right--;
+                } else {
+                    List<Integer> triplet = Arrays.asList(nums[left], nums[mid], nums[right]);
+                    triplets.add(triplet);
+                    mid++;
+                }
+            }
+        }
+        return new ArrayList<>(triplets);
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {-1,0,1,2,-1,-4};
+        _3Sum temp = new _3Sum();
+        List<List<Integer>> result = temp.threeSum(nums);
+        System.out.println(result);
+    }
+}
